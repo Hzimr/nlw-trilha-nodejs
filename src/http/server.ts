bfef77162541644222423418a2e228f32962c9da
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-13 12:47:47 -0300

    feat: created cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,17 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie'
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { secretkey } from "../utils/secretWord";
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: `${secretkey}`,
+  hook: 'onRequest',
+  parseOptions: {}
+})
 
 app.register(createPoll)
 app.register(getPoll)
