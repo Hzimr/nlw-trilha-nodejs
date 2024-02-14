8059dfc7e0c0915932239b804af12451ae9dd281
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-14 17:45:36 -0300

    example: connection with websocket

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,5 +1,14 @@
 import { FastifyInstance } from "fastify";
+import { request } from "http";
 
 export async function pollResults(app: FastifyInstance) {
-  app.get('/polls/:pollId/results', { websocket: true })
+  app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
+    connection.socket.on('message', (message: string) => {
+      connection.socket.send('You sent: ' + message)
+
+      setInterval(() => {
+        connection.socket.send('oi')
+      }, 500)
+    })
+  })
 }
